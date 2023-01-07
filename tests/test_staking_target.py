d7e833b2e2c41b373cc2624bcffdbe30828dfa6e
Matthias Zimmermann <zimmermann@xbrain.ch>
2023-01-07 16:42:41 +0000

    add failure mode tests for staking target handling

--- tests/test_staking_target.py
+++ tests/test_staking_target.py
@@ -4,8 +4,10 @@ import pytest
 from brownie.network.account import Account
 from brownie import (
     chain,
+    web3,
     BundleRegistry,
     Staking,
+    USD2,
     DIP,
 )
 
@@ -26,6 +28,7 @@ def test_targets_happy_path(
     bundleRegistry: BundleRegistry,
     staking: Staking,
     stakerWithDips: Account,
+    usd2: USD2,
     dip: DIP,
 ):
     instance_id = instanceService.getInstanceId()
@@ -116,10 +119,24 @@ def test_targets_happy_path(
 
     assert staking.targets() == 0
 
-    # register instance target
-    staking.register(iid, instance_target)
+    # repeat toTarget, difference to before: component and 
+    # bundle are now registered in the registry, in this on-chain
+    # scenario toTarget will also fill in token attributes
+    (cid, component_target) = staking.toTarget(
+        type_component, instance_id, riskpool_id, 0, '')
 
-    # TODO check log entry (LogStakingTargetRegistered)
+    (bid, bundle_target) = staking.toTarget(
+        type_bundle, instance_id, riskpool_id, bundle_id, '')
+
+    # register instance target
+    tx = staking.register(iid, instance_target)
+
+    assert 'LogStakingTargetRegistered' in tx.events
+    assert tx.events['LogStakingTargetRegistered']['targetId'] == iid
+    assert tx.events['LogStakingTargetRegistered']['targetType'] == type_instance
+    assert tx.events['LogStakingTargetRegistered']['instanceId'] == instance_id
+    assert tx.events['LogStakingTargetRegistered']['componentId'] == 0
+    assert tx.events['LogStakingTargetRegistered']['bundleId'] == 0
 
     assert staking.isTarget(instance_target_id) is True
     assert staking.isTarget(component_target_id) is False
@@ -127,9 +144,14 @@ def test_targets_happy_path(
     assert staking.targets() == 1
 
     # register component target
-    staking.register(cid, component_target)
+    tx = staking.register(cid, component_target)
 
-    # TODO check log entry
+    assert 'LogStakingTargetRegistered' in tx.events
+    assert tx.events['LogStakingTargetRegistered']['targetId'] == cid
+    assert tx.events['LogStakingTargetRegistered']['targetType'] == type_component
+    assert tx.events['LogStakingTargetRegistered']['instanceId'] == instance_id
+    assert tx.events['LogStakingTargetRegistered']['componentId'] == riskpool_id
+    assert tx.events['LogStakingTargetRegistered']['bundleId'] == 0
 
     assert staking.isTarget(instance_target_id) is True
     assert staking.isTarget(component_target_id) is True
@@ -137,20 +159,60 @@ def test_targets_happy_path(
     assert staking.targets() == 2
 
     # register bundle target
-    staking.register(bid, bundle_target)
+    tx = staking.register(bid, bundle_target)
 
-    # TODO check log entry
+    assert 'LogStakingTargetRegistered' in tx.events
+    assert tx.events['LogStakingTargetRegistered']['targetId'] == bid
+    assert tx.events['LogStakingTargetRegistered']['targetType'] == type_bundle
+    assert tx.events['LogStakingTargetRegistered']['instanceId'] == instance_id
+    assert tx.events['LogStakingTargetRegistered']['componentId'] == riskpool_id
+    assert tx.events['LogStakingTargetRegistered']['bundleId'] == bundle_id
 
     assert staking.isTarget(instance_target_id) is True
     assert staking.isTarget(component_target_id) is True
     assert staking.isTarget(bundle_target_id) is True
     assert staking.targets() == 3
 
-    # TODO check log entry
+    # check getTargetId for instance, riskpool and bundle targets
+    assert staking.getTargetId(0) == iid
+    assert staking.getTargetId(1) == cid
+    assert staking.getTargetId(2) == bid
 
-    # TODO check getTargeteId for instance, riskpool and bundle targets
+    # check getTarget for instance, riskpool and bundle targets
+    zero_address = '0x0000000000000000000000000000000000000000'
+    zero_data = '0x'
 
-    # TODO check getTarget for instance, riskpool and bundle targets
+    itarget = staking.getTarget(iid).dict()
+
+    assert itarget['targetType'] == type_instance
+    assert itarget['instanceId'] == instance_id
+    assert itarget['componentId'] == 0
+    assert itarget['bundleId'] == 0
+    assert itarget['data'] == zero_data
+    assert itarget['token'] == zero_address
+    assert itarget['chainId'] == 0
+
+    # check getTarget for riskpool
+    ctarget = staking.getTarget(cid).dict()
+
+    assert ctarget['targetType'] == type_component
+    assert ctarget['instanceId'] == instance_id
+    assert ctarget['componentId'] == riskpool_id
+    assert ctarget['bundleId'] == 0
+    assert ctarget['data'] == zero_data
+    assert ctarget['token'] == usd2
+    assert ctarget['chainId'] == web3.chain_id
+
+    # check getTarget for bundle target
+    btarget = staking.getTarget(bid).dict()
+
+    assert btarget['targetType'] == type_bundle
+    assert btarget['instanceId'] == instance_id
+    assert btarget['componentId'] == riskpool_id
+    assert btarget['bundleId'] == bundle_id
+    assert btarget['data'] == zero_data
+    assert btarget['token'] == usd2
+    assert btarget['chainId'] == web3.chain_id
 
 
 def test_targets_failure_modes(
@@ -166,19 +228,47 @@ def test_targets_failure_modes(
 ):
     instance_id = instanceService.getInstanceId()
     riskpool_id = riskpool.getId()
-    bundle_name = 'bundle-1'
-    bundle_id = new_bundle(
-        instance,
-        instanceOperator,
-        investor,
-        riskpool,
-        bundle_name)
 
     # register token, instance, component (not bundle, yet)
     token = instanceService.getComponentToken(riskpool_id)
     bundleRegistry.registerToken(token)
     staking.setDipContract(dip)
-    bundle = riskpool.getBundleInfo(bundle_id).dict()
-    bundle_expiry_at = bundle['createdAt'] + bundle['lifetime']
+
+    type_protocol = 1
+    type_instance = 2
+    type_component = 3
+
+    (iid, instance_target) = staking.toTarget(
+        type_instance, instance_id, 0, 0, '')
+
     bundleRegistry.registerInstance(instance.getRegistry())
-    bundleRegistry.registerComponent(instance_id, riskpool_id)
+    
+    # register instance target
+    staking.register(iid, instance_target)
+
+    with brownie.reverts('ERROR:STK-010:TARGET_INDEX_TOO_LARGE'):
+        target = staking.getTargetId(1)
+
+    (cid, component_target) = staking.toTarget(
+        type_component, instance_id, riskpool_id, 0, '')
+
+    # try to get target metadata for a non-registred target
+    with brownie.reverts('ERROR:STK-003:TARGET_NOT_REGISTERED'):
+        staking.getTarget(cid)
+
+    # try to register protocol target (not (yet) supported)
+    with brownie.reverts('ERROR:STK-019:TARGET_TYPE_UNSUPPORTED'):
+        (pid, protocol_target) = staking.toTarget(
+            type_protocol, 0, 0, 0, '')
+
+    # try to re-register instance target
+    with brownie.reverts('ERROR:STK-010:TARGET_ALREADY_REGISTERED'):
+        staking.register(iid, instance_target)
+
+    # try to register target with a non-matching id
+    with brownie.reverts('ERROR:STK-011:TARGET_DATA_INCONSISTENT'):
+        staking.register(cid, instance_target)
+
+    # try to register component target (without having registered component first)
+    with brownie.reverts('ERROR:STK-012:TARGET_NOT_IN_REGISTRY'):
+        staking.register(cid, component_target)
