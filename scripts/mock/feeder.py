2b91c1ebf1174633a85e7c8f76410d977868a979
Matthias Zimmermann <zimmermann@xbrain.ch>
2023-01-13 15:55:56 +0000

    add price feed state handling

--- scripts/mock/feeder.py
+++ scripts/mock/feeder.py
@@ -3,7 +3,11 @@ import sched
 import time
 
 from threading import Thread
-from fastapi import FastAPI
+
+from fastapi import (
+    FastAPI,
+    HTTPException,
+)
 
 # usage
 # 1. open new terminal
@@ -13,27 +17,64 @@ from fastapi import FastAPI
 # reg 2: uvicorn scripts.mock.feeder:app --reload
 # reg 4: curl localhost:8000/
 
+# price feed states
+STABLE = 'stable'
+TRIGGERED = 'triggered'
+DEPEGGED = 'depegged'
+STATES = [STABLE, TRIGGERED, DEPEGGED]
+
+# scheduler
 PRIORITY = 1
 INTERVAL = 5 # seconds to wait for new data
 
 # the api server
 app = FastAPI()
 
+# pricefeed stuff
+state = STABLE
+
 # scheduling stuff
 schedule = sched.scheduler(time.time, time.sleep)
-next_event = None
-events = 0
+next_round = None
+rounds = 0
 
 
 @app.get('/')
 async def root():
+    return { 
+        'info': 'price feed api server',
+        'openapi': 'localhost:8000/docs',
+        'state': state,
+        }
+
+@app.get('/state')
+async def get_state():
     return {
-        'message': 'hello world!'
+        'state': state
+        }
+
+@app.put('/state/{new_state}')
+async def set_state(new_state:str):
+    global state
+
+    if new_state not in STATES:
+        raise HTTPException(
+            status_code=404, 
+            detail="state {} invalid. valid states: {}".format(
+                new_state, ','.join(STATES)
+            ))
+
+    old_state = state
+    state = new_state;
+
+    return {
+        'old_state': old_state,
+        'new_state': new_state
         }
 
 
 def execute_event():
-    logging.info('events <<{}>>'.format(events))
+    logging.info('rounds <<{}>>'.format(rounds))
 
 
 @app.on_event('startup')
@@ -54,24 +95,24 @@ def shutdown_event():
     global schedule
 
     logging.info('stopping scheduler')
-    if schedule and next_event:
-        schedule.cancel(next_event)
+    if schedule and next_round:
+        schedule.cancel(next_round)
 
     logging.info('scheduler stopped')
 
 
 def start_scheduler():
-    global next_event
+    global next_round
 
-    next_event = schedule.enter(INTERVAL, PRIORITY, schedule_event, (schedule,))
+    next_round = schedule.enter(INTERVAL, PRIORITY, schedule_round, (schedule,))
     logging.info('scheduler started')
     schedule.run()
 
 
-def schedule_event(s):
-    global events
-    global next_event
+def schedule_round(s):
+    global rounds
+    global next_round
 
-    events += 1
+    rounds += 1
     execute_event()
-    next_event = schedule.enter(INTERVAL, PRIORITY, schedule_event, (s,))
+    next_round = schedule.enter(INTERVAL, PRIORITY, schedule_round, (s,))
