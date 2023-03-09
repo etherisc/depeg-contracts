7982bae0f65c004edb63425a48def7d5d78c7dc6
Marc Doerflinger <marc@doerf.li>
2023-03-09 12:44:38 +0000

    disable staking in test deployment

--- scripts/test_deployment.py
+++ scripts/test_deployment.py
@@ -4,4 +4,4 @@ def main():
     all_in_1(
         stakeholders_accounts=None,
         registry_address='0x345cA3e014Aaf5dcA488057592ee47305D9B3e10',
-        disable_staking=False)
+        disable_staking=True)
